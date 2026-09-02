c3d057b25452abe5ff670ca906d44239753ae61e
abinaya0407-abi <abinayarajendran0407@gmail.com>
2026-09-02 13:26:23 +0530

    Add INNER JOIN query for Student and Department
    Added INNER JOIN query to display student names with their corresponding department names.

--- starter.sql
+++ starter.sql
@@ -1,14 +1,7 @@
--- Student-Department INNER JOIN Assignment
-
--- STEP 1: Create Department table
-
 CREATE TABLE Department (
     DepartmentID INT PRIMARY KEY,
     DepartmentName VARCHAR(100)
 );
-
-
--- STEP 2: Create Student table
 
 CREATE TABLE Student (
     StudentID INT PRIMARY KEY,
@@ -16,17 +9,11 @@ CREATE TABLE Student (
     DepartmentID INT
 );
 
-
--- STEP 3: Insert Department records
-
 INSERT INTO Department (DepartmentID, DepartmentName)
 VALUES
 (101, 'Computer Science'),
 (102, 'Mathematics'),
 (103, 'Physics');
-
-
--- STEP 4: Insert Student records
 
 INSERT INTO Student (StudentID, StudentName, DepartmentID)
 VALUES
@@ -35,7 +22,9 @@ VALUES
 (1003, 'Karthik', 101),
 (1004, 'Nisha', 103);
 
-
--- STEP 5: Perform INNER JOIN
--- Display Student Name with Department Name.
--- Write your INNER JOIN query below:
+SELECT
+    Student.StudentName,
+    Department.DepartmentName
+FROM Student
+INNER JOIN Department
+    ON Student.DepartmentID = Department.DepartmentID;
